ebfdc4ed14c484161f1691724e5f29549384194b
Ran Bao <rba90@uclive.ac.nz>
2014-09-30 10:48:08 +1300

    Initial Commit

---
+++ OpenGL_Bouncing_Ball/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL_Bouncing_Ball
+//
+//  Created by Ran Bao on 30/09/14.
+//  Copyright (c) 2014 R&B. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
